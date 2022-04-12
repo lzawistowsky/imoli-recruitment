1fdeac67be9c54a45405e48388e137cf8401e1e4
lzawistowsky <63401740+lzawistowsky@users.noreply.github.com>
2022-04-12 20:20:35 +0200

    feat: film model

---
+++ src/models/film.ts
@@ -0,0 +1,17 @@
+import { Schema, Types, model } from 'mongoose';
+
+interface IFilm {
+  title: string
+  releaseDate: string
+  characterList: Types.DocumentArray<Types.ObjectId>
+}
+
+const filmSchema = new Schema<IFilm>({
+  title: { type: String, required: true },
+  releaseDate: { type: String, required: true },
+  characterList: [ { type: Schema.Types.ObjectId, ref: 'Character'} ]
+})
+
+const Film = model<IFilm>('Film', filmSchema);
+
+export default Film
