6ec0b36f3b0998f6a3eb98607546bd6cc104fcde
lzawistowsky <63401740+lzawistowsky@users.noreply.github.com>
2022-04-15 21:10:57 +0200

    feat: heroku settings

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import helmet from 'helmet'
 import 'dotenv/config'
 
 import filmsRoutes from './routes/films'
@@ -7,13 +8,14 @@ import favoritesRoutes from './routes/favorites'
 
 const app = express()
 
+app.use(helmet())
 app.use(express.json())
 
 app.use(filmsRoutes)
 app.use(favoritesRoutes)
 
 mongoose.connect(process.env.DB_CONNECT as string).then(res => {
-    app.listen(process.env.PORT)
+    app.listen(process.env.PORT || 8080)
 }).catch(err => {
     console.log(err)
 })
